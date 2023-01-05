7d7995212bad62e296a5c70c03d78a3bdedb0567
tsaolun <ycl18@live.com>
2023-01-05 15:06:47 +0800

    修改：     src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,22 @@
-use yew::prelude::*;
+use yew::{prelude::*, virtual_dom};
 use gloo::console::log;
 use stylist::{yew::styled_component, style};
 
 #[styled_component(App)]
 pub fn app() -> Html {
+    html!{
+        <>
+            <Head content={"记一些物事，不一定是对的。"}/>
+        </>
+    }
+}
+#[derive(Properties, PartialEq)]
+pub struct HeadProps {
+    pub content: virtual_dom::AttrValue,
+}
+
+#[function_component]
+fn Head(props: &HeadProps) -> Html {
     log!(">> 个人联系方式: ycl18@live.com");
     let stylesheet = style!(
         r#"
@@ -21,7 +34,7 @@ pub fn app() -> Html {
     html!{
         <div class={stylesheet}>
             <h1>{"bloq"}</h1>
-            <p>{"记一些物事，不一定是对的。"}</p>
+            <p>{props.content.clone()}</p>
         </div>
     }
 }
